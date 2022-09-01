f33b96f8b45666632b5784509aa065eec37dade4
Amr Safwat <amrsafwat02@gmail.com>
2022-09-01 02:58:28 +0200

    added code for javascript

--- main.js
+++ main.js
@@ -2,5 +2,16 @@ let icon = document.querySelector('.toggle-menu');
 let ul = document.querySelector('nav ul');
 
 icon.onclick = () => {
-  ul.classList.toggle('show');
-}
+	ul.classList.toggle('show');
+};
+
+let portLi = document.querySelectorAll('.portfolio ul li');
+
+portLi.forEach((el) => {
+	el.onclick = () => {
+		portLi.forEach((el) => {
+			el.classList.remove('active');
+		});
+			el.classList.add('active');
+	};
+});
