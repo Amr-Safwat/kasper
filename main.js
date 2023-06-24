a0ff88c3981934e3ac2328b2db411b430afd71a3
Amr-Safwat <amrsafwat02@gmail.com>
2023-06-24 18:14:08 +0200

    Image slider created

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+// Change background for landing
+let landing = document.querySelector('.landing');
+
+currItem = 2;
+
+let leftBtn = document.querySelector('.landing .left');
+let rightBtn = document.querySelector('.landing .right');
+
+leftBtn.onclick = function () {
+	if (currItem != 1) {
+		currItem--;
+		check();
+	}
+};
+rightBtn.onclick = function () {
+	if (currItem != 3) {
+		currItem++;
+		check();
+	}
+};
+
+let bullets = document.querySelectorAll('.bullets li');
+
+check();
+
+function check() {
+	if (currItem == 1) {
+		landing.style.backgroundImage = "url('../images/landing1.jpg')";
+	} else if (currItem == 2) {
+		landing.style.backgroundImage = "url('../images/landing2.jpg')";
+	} else if (currItem == 3) {
+		landing.style.backgroundImage = "url('../images/landing3.jpg')";
+	}
+
+	bullets.forEach((bullet) => {
+		bullet.classList.remove('active');
+	});
+	bullets[currItem - 1].classList.add('active');
+}
+
 let icon = document.querySelector('.toggle-menu');
 let ul = document.querySelector('nav ul');
 
@@ -12,6 +52,6 @@ portLi.forEach((el) => {
 		portLi.forEach((el) => {
 			el.classList.remove('active');
 		});
-			el.classList.add('active');
+		el.classList.add('active');
 	};
 });
