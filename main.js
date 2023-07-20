82cc3e3c04394a0d7696cd418fca190dd811bc70
Amr-Safwat <amrsafwat02@gmail.com>
2023-07-20 12:56:10 +0300

    Adedd animations

--- main.js
+++ main.js
@@ -37,6 +37,38 @@ function check() {
 	});
 	bullets[currItem - 1].classList.add('active');
 }
+	// Animation
+	let lText = document.querySelector('.landing .text');
+	let dText = document.querySelector('.design .text');
+	let design = document.querySelector('.design');
+	let skills = document.querySelector('.skills');
+	let spans = document.querySelectorAll('.prog span');
+	let numbers = document.querySelectorAll('.stats .number');
+	let stats = document.querySelector('.stats');
+	
+	lText.style.left = '0';
+
+	window.onscroll = function () {
+		if(this.scrollY > design.offsetTop) {
+			dText.style.right = '0';
+		}
+
+		if(this.scrollY > skills.offsetTop) {
+			spans.forEach((span)=> {
+				span.style.width = span.dataset.prograss;
+			});
+		}
+
+		if(this.scrollY > stats.offsetTop) {
+			numbers.forEach((number)=> {
+				for(let i = 0; i <= number.dataset.num; i++) {
+					number.innerHTML = i;
+				}
+			});
+		}
+	}
+
+	// Animation
 // Change background for landing
 
 let icon = document.querySelector('.toggle-menu');
